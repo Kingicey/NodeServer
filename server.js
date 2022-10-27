5da13e0dd48d24e423339584ec81e3d3e7d55fa6
Kingicey <iceyokolo@gmail.com>
2022-10-27 16:52:49 +0100

    third commit

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const hostname = "0.0.0.0";
 const port = 8000;
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("", (req, res) => {
   res.json({
     slackUsername: "Okolo Ifeanyi Christian",
     backend: true,
-    age: 37,
+    age: 30,
     bio: "I am a backend development looking forward to collaborate. I enjoy analyzing and solving problems",
   });
 });
